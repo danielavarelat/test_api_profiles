e4bc36c907654b851f1d9a74b21b955837306c93
danielavarelat <danielavarelatabares@gmail.com>
2020-09-18 15:38:08 -0500

    all working with db atlas

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pymongo
 from pymongo import MongoClient
 from flask import Flask, request, render_template
 from flask_pymongo import PyMongo
+from itertools import groupby
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,8 @@ client = MongoClient(
 db = client['profiles']
 collection_student = db['student_coll']
 collection_course = db['course_coll']
+dbfavs = client['favorites']
+collection_favs = dbfavs['favoritescoll']
 
 # Atlas: daniela, crispeta
 @app.route("/studentProfile/<idStudent>",  methods=['GET', 'POST'])
@@ -47,50 +51,70 @@ def form(idStudent=None):
 
 @app.route("/favorites/<idCourse>",  methods=['GET', 'POST'])
 def favs(idCourse=None):
+    list_sec = ["Generales"]
+    ids = ["0"]
     cursename = str(request.args.get('coursename'))
     sections = str(request.args.get('section'))
-    print(sections)
-
-    # list_rows = [0 for i in list_rows if i == 1]
-    list_list_titles = [["hola", "hola2", "hola3"], [
-        "second", "secondsito"], ["jijij"], ["lol"]]
-    list_list_titles = [[], [], [], []]
-    list_rows = [len(i) for i in list_list_titles]
-    list_rows = [1 for i in list_rows if i == 0]
-    print(list_rows)
-    list_sec = ["Generales"]
     if sections != "None":
         if sections[-1] == ",":
             sections = sections[:-1]
         for sec in sections.split(","):
             sec = sec.split("--")
             list_sec.append("Section id " + sec[0] + ": " + sec[1])
-    if request.method == "POST":
-        counter = str(request.form.getlist("test"))
-        print("Counter " + counter)
+            ids.append(str(sec[0]))
+    q = collection_favs.find({"courseid": idCourse})
+    result = list(q)
 
-        if counter != "[]":
-            rows = []
-            for i in counter.replace("[", "").replace("]", "").split(","):
-                rows.append(int(i.replace("/", "").replace("'", '')))
-            max_rows = max(rows) + 2
-        else:
-            max_rows = 1
-        print("max rows " + str(max_rows))
-        res = []
-        for i in range(max_rows):
-            if i == 0:
-                x = request.form.getlist("title")
-            else:
-                x = request.form.getlist("title{}".format(i-1))
-            res.append(x)
-        print("res " + str(res))
+    if request.method == "POST":
+        print("POST")
+        ids = (request.args.get('idsSections')).split(",")  # get ids from url
+        dict_value_titles = {}  # {numsections : list of titles}
+        dict_value_urls = {}
+        for i in range(len(ids)):
+            x = request.form.getlist("title{}".format(i))
+            y = request.form.getlist("url{}".format(i))
+            dict_value_titles[ids[i]] = x
+            dict_value_urls[ids[i]] = y
+        l = []
+        for i in dict_value_titles.keys():
+            titles = dict_value_titles[i]
+            urls = dict_value_urls[i]
+            section = []
+            for t in range(len(titles)):
+                section.append(
+                    {"courseid": idCourse, "sectionid": i, "title": titles[t], "url": urls[t]})
+            l.append(section)
+        flat_list = [item for sublist in l for item in sublist]
+        # Insert in mongo DB
+        print(flat_list)
+        if result:  # If no hay nada de ese curso en la db
+            collection_favs.delete_many({"courseid": idCourse})
+        if flat_list:
+            collection_favs.insert_many(flat_list)
         return render_template("favorites_ok.html")
 
     if request.method == "GET":
+        result.sort(key=lambda x: x['sectionid'])
+        grouped_dict = {}
+        for k, v in groupby(result, key=lambda x: x['sectionid'][:7]):
+            grouped_dict[k] = list(v)
+        list_list_titles = []
+        list_list_urls = []
+        for i in ids:
+            if grouped_dict.get(i):
+                list_list_titles.append([doc['title']
+                                         for doc in grouped_dict.get(i)])
+                list_list_urls.append([doc['url']
+                                       for doc in grouped_dict.get(i)])
+            else:
+                list_list_titles.append([])
+                list_list_urls.append([])
+        list_rows = [len(i) for i in list_list_titles]
+        list_rows = [1 if x == 0 else x for x in list_rows]
+        print(list_list_titles)
         return render_template("favorites.html", idCourse=idCourse, cursename=cursename,
-                               number=len(list_sec), sections=list_sec, nrows=list_rows, titles=list_list_titles)
-    return render_template("favorites.html", idCourse=idCourse, cursename=cursename, number=len(list_sec), sections=list_sec)
+                               number=len(list_sec), sections=list_sec, nrows=list_rows, titles=list_list_titles, urls=list_list_urls, ids=",".join(ids))
+    return render_template("favorites.html", idCourse=idCourse, cursename=cursename, number=len(list_sec), sections=list_sec, ids=",".join(ids))
 
 
 @app.route("/courseProfile/<idCourse>",  methods=['GET', 'POST'])
